05721aec468b192d88fb306c8eb0a5e52e0319eb
Miodrag Mijatović <fifajfom@gmail.com>
2019-04-12 19:52:54 +0200

    ubacena osnova

---
+++ Dinar.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+namespace Kalkulator_Izvrsitelj
+{
+    class Dinar
+    {
+        MainWindow y;
+        public Dinar(MainWindow form)
+        {
+            y = form;
+        }
+
+        public void OsnovniSud()
+        {
+            // osnovni sud
+            double os;
+            var dug = Convert.ToInt64(y.Dug.Text);
+            if (dug > 0 && dug <= 10000)
+            {
+                os = 1900 / 3D;
+                var rrr = Convert.ToString(System.Math.Round(os, 2));
+                y.sudskataksa.Text = rrr;
+            }
+            if (dug > 10000 && dug <= 100000)
+            {
+
+                os = (1900D + dug / 100D * 4D) / 3D;
+                var rrr = Convert.ToString(System.Math.Round(os, 2));
+                y.sudskataksa.Text = rrr;
+            }
+            if (dug > 100000 && dug <= 500000)
+            {
+
+                os = (9800D + dug / 100D * 2D) / 3D;
+                var rrr = Convert.ToString(System.Math.Round(os, 2));
+                y.sudskataksa.Text = rrr;
+            }
+            if (dug > 500000 && dug <= 1000000)
+            {
+
+                os = (29300D + dug / 100D * 1D) / 3D;
+                var rrr = Convert.ToString(System.Math.Round(os, 2));
+                y.sudskataksa.Text = rrr;
+            }
+            if (dug > 1000000)
+            {
+                os = (48800D + dug / 100D * 0.5D) / 3D;
+                if (os >= 97500)
+                {
+                    os = 32500;
+                    var rrr = Convert.ToString(System.Math.Round(os, 2));
+                    y.sudskataksa.Text = rrr;
+                }
+                else
+                {
+                    var rrr = Convert.ToString(System.Math.Round(os, 2));
+                    y.sudskataksa.Text = rrr;
+                }
+            }
+            if (dug <= 0)
+            {
+                string message = "Uneli ste neodgovarajucu vrednost";
+                string title = "Greska";
+                MessageBox.Show(message, title);
+            }
+        }
+
+        public void PrivredniSud()
+        {
+
+            double os;
+            var dug = Convert.ToInt64(y.Dug.Text);
+            if (dug > 0 && dug <= 10000)
+            {
+                os = 1300 / 3D;
+                var rrr = Convert.ToString(System.Math.Round(os, 2));
+                y.sudskataksa.Text = rrr;
+            }
+            if (dug > 10000 && dug <= 100000)
+            {
+
+                os = (3900D + dug / 100D * 6D) / 3D / 3D;
+                var rrr = Convert.ToString(System.Math.Round(os, 2));
+                y.sudskataksa.Text = rrr;
+            }
+            if (dug > 100000 && dug <= 1000000)
+            {
+
+                os = (15600D + dug / 100D * 2D) / 3D / 3D;
+                var rrr = Convert.ToString(System.Math.Round(os, 2));
+                y.sudskataksa.Text = rrr;
+            }
+            if (dug > 1000000 && dug <= 10000000)
+            {
+
+                os = (54600D + dug / 100D * 1D) / 3D / 3D;
+                var rrr = Convert.ToString(System.Math.Round(os, 2));
+                y.sudskataksa.Text = rrr;
+            }
+            if (dug > 1000000)
+            {
+                os = (249600D + dug / 100D * 0.5D) / 3D / 3;
+                if (os >= 390000)
+                {
+                    os = 43333;
+                    var rrr = Convert.ToString(System.Math.Round(os, 2));
+                    y.sudskataksa.Text = rrr;
+                }
+                else
+                {
+                    var rrr = Convert.ToString(System.Math.Round(os, 2));
+                    y.sudskataksa.Text = rrr;
+                }
+            }
+            if (dug <= 0)
+            {
+                string message = "Uneli ste neodgovarajucu vrednost";
+                string title = "Greska";
+                MessageBox.Show(message, title);
+            }
+        }
+
+        public void Math()
+        {
+            if (y.Dug.Text != "")
+            {
+                if (y.Osnovni.IsSelected == true)
+                {
+
+                    OsnovniSud();
+                }
+                else
+                {
+
+                    PrivredniSud();
+                }
+            }
+            else { }
+        }
+
+
+    }
+}
---
+++ Kurs.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Kalkulator_Izvrsitelj
+{
+    class Kurs
+    {
+        MainWindow y;
+        public Kurs(MainWindow form)
+        {
+            y = form;
+        }
+
+        public static bool IsInternetConnection()  // Proverava da li ima konekcije 
+        {
+            System.Net.WebRequest req = System.Net.WebRequest.Create("https://www.google.co.in/");
+            System.Net.WebResponse resp;
+            try
+            {
+                resp = req.GetResponse();
+                resp.Close();
+                req = null;
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public static void Update() //update kursa
+        {
+
+
+            bool result = Kurs.IsInternetConnection();  //Ukoliko ima net-a radi update, ukoliko ne iscitava prethodne vrednosti
+            if (result == true)
+            {
+
+                string htmlCode = "";
+                using (WebClient client = new WebClient())
+                {
+                    client.Headers.Add(HttpRequestHeader.UserAgent, "AvoidError");
+                    htmlCode = client.DownloadString("https://www.nbs.rs/kursnaListaModul/srednjiKurs.faces?lang=lat");
+                }
+                HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+
+                doc.LoadHtml(htmlCode);
+
+                DataTable dt = new DataTable();
+                dt.Columns.Add("Name", typeof(string));
+
+
+                int count = 0;
+
+                using (var file = new StreamWriter("test.txt"))
+                {
+                    foreach (var row in doc.DocumentNode.SelectNodes("//table[@id='index:srednjiKursLista']/tbody/tr"))
+                    {
+                        DataRow dr = dt.NewRow();
+                        foreach (var cell in row.SelectNodes("td"))
+                        {
+                            if ((count % 1 == 0))
+                            {
+                                dr["Name"] = cell.InnerText.Replace("&nbsp;", " ");
+                            }
+                            else
+                            {
+                            }
+
+                        }
+                        file.WriteLine(dr[0]);
+
+                    }
+
+                }
+            }
+            else { }
+        }
+
+        public void KursMain()
+        {
+
+            Update();
+            string[] lines = System.IO.File.ReadAllLines("test.txt");
+
+            y.keur.Text = "EUR: " + lines[0];
+            y.kusd.Text = "USD: " + lines[15];
+            y.kchf.Text = "CHF: " + lines[13];
+
+
+        }
+
+        //public void nap()
+        //{
+        //    if (System.Text.RegularExpressions.Regex.IsMatch(y.Naplata.Text, "[^0-9]"))
+        //    {
+        //        MessageBox.Show("Dozvoljen je unos samo brojeva");
+        //        y.Naplata.Text = y.Naplata.Text.Remove(y.Naplata.Text.Length - 1);
+        //    }
+        //}
+        public void Dug()
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(y.Dug.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Dozvoljen je unos samo brojeva");
+                y.Dug.Text = y.Dug.Text.Remove(y.Dug.Text.Length - 1);
+            }
+        }
+
+
+    }
+}
